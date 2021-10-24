769e915dbc3868623677abf4627c480c538f2bef
arichild <arieevq@gmail.com>
2021-10-24 11:43:14 +0300

    save

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const section = document.querySelector('section');
 const progressBar = document.querySelector('#progress-bar');
 const btn = document.querySelector('.light');
+const btntoTop = document.querySelector('.up')
 
 btn.addEventListener('click', changeTheme);
 
@@ -9,25 +10,27 @@ function changeTheme() {
 
     if(getClass === 'light') {
         btn.setAttribute('class', 'dark');
+        btntoTop.setAttribute('class', 'toTop dark');
         btn.textContent = 'DARK';
 
         document.body.style.backgroundColor = '#fff';
         section.style.color = 'black';
     } else if(getClass === 'dark') {
         btn.setAttribute('class', 'light');
+        btntoTop.setAttribute('class', 'toTop');
         btn.textContent = 'LIGHT';
 
         document.body.style.backgroundColor = '#242423';
         section.style.color = 'white';
     }
 
+    localStorage.getItem('class');
+
     document.body.style.transition = 'all 1.2s';
 }
 
 const animateProgressBar = () => {
     let scrollDistance = -section.getBoundingClientRect().top;
-
-    console.log(section.getBoundingClientRect());
 
     let progressWidth = (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
 
@@ -41,3 +44,10 @@ const animateProgressBar = () => {
 }
 
 window.addEventListener('scroll', animateProgressBar);
+
+btntoTop.addEventListener('click', toTop);
+
+function toTop() {
+    window.scrollTo(0, 0);
+    document.body.style.transition = 'all 1.2s';
+}
